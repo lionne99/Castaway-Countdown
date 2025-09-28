f861bbba9483b83cd3efe488dca056199bd9fc87
lionne99 <ashwinandan@gmail.com>
2025-09-28 07:23:37 -0400

    Finished the game

--- game2.py
+++ game2.py
@@ -7,15 +7,11 @@ import math
 WIDTH, HEIGHT = 800,600
 MAP_W, MAP_H = 1200, 900
 FPS = 60
-START_TIME = 100  # seconds to survive
+START_TIME = 60  # seconds to survive
 RAFT_MATERIALS = 20  # materials needed to win
 planksInQuest1 = 2
 planksInQuest2 = 4
 planksInQuest3 = 6
-
-firstTime1 = True
-firstTime2 = True
-firstTime3 = True
 
 
 
@@ -28,7 +24,6 @@ pygame.display.set_caption("Castaway Countdown")
 runTitlescreen = True
 runGame = False
 runEndscreen = False
-runWinScreen = False
 questScreen = False
 runQuest1 = False
 runQuest2 = False
@@ -49,27 +44,6 @@ quest3_started = False
 clock = pygame.time.Clock()
 font = pygame.font.SysFont(None, 40)
 
-#Sign background
-menuBKG = pygame.image.load("Menu.png").convert()
-menuBKG = pygame.transform.scale(menuBKG, (WIDTH, HEIGHT))
-
-#Start background
-startBKG = pygame.image.load("startBKG.png").convert()
-startBKG = pygame.transform.scale(startBKG, (WIDTH, HEIGHT))
-
-#Battle background
-grassBKG = pygame.image.load("grassBKG1.png").convert()
-grassBKG = pygame.transform.scale(grassBKG, (WIDTH, HEIGHT))
-
-#Win background
-winBKG = pygame.image.load("winBKG.png").convert()
-winBKG = pygame.transform.scale(winBKG, (WIDTH, HEIGHT))
-
-#Lose background
-loseBKG = pygame.image.load("loseBKG.png").convert()
-loseBKG = pygame.transform.scale(loseBKG, (WIDTH, HEIGHT))
-
-#Game background
 background = pygame.image.load("Game background dd.png").convert()
 background = pygame.transform.scale(background, (MAP_W, MAP_H))
 
@@ -82,9 +56,9 @@ button_rect = pygame.Rect(WIDTH//2 - 100, HEIGHT//2 - 40, 200, 80)
 #sprite stuff 
 
 class dock(pygame.sprite.Sprite):
-    def __init__(self, x, y, sizeX, sizeY):
+    def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.image.load("Dock.png").convert_alpha()
+        self.image = pygame.image.load("png").convert_alpha()
         # Resize the image
         self.image = pygame.transform.scale(self.image, (sizeX, sizeY))
         self.rect = self.image.get_rect()
@@ -198,7 +172,7 @@ class Button:
             color = self.color
 
         # Draw rectangle
-        pygame.draw.rect(surface, color, self.rect, border_radius= 10)
+        pygame.draw.rect(surface, color, self.rect, border_radius=10)
 
         # Draw text centered inside
         text_surf = self.font.render(self.text, True, self.text_color)
@@ -217,21 +191,21 @@ class Button:
         )
 
 
-
-dock = dock(WIDTH//2, HEIGHT//2 + 170, 400, 400)
-
 player = Character(100, 100)
-
-
-
+bouncer1 = BouncingSprite("Hunter.png", 100, 100, 4, 2)
+bouncer2 = BouncingSprite("Hunter.png", 300, 200, -5, 3)
+bouncer3 = BouncingSprite("Hunter.png", 100, 200, 2,6)
+bouncer4 = BouncingSprite("Hunter.png", 300, 100, -3, 4)
+bouncer5 = BouncingSprite("Hunter.png", 400, 200, 4, -4)
+bouncer6 = BouncingSprite("Hunter.png", 300, 100, 3,4)
+bouncer7 = BouncingSprite("Hunter.png", 100, 400, -2, 3)
+bouncer8 = BouncingSprite("Hunter.png", 300, 200, 4, -1)
 
 player.rect.center = (MAP_W // 2, MAP_H // 2)
 
 all_wood_Quest1 = pygame.sprite.Group()
 all_wood_Quest2 = pygame.sprite.Group()
 all_wood_Quest3 = pygame.sprite.Group()
-
-
 
 def fillPlankGroup(numPlanks, plankGroup):
     for i in range(numPlanks):
@@ -253,16 +227,6 @@ def fillPlankGroup(numPlanks, plankGroup):
                 break
 
 fillPlankGroup(planksInQuest1, all_wood_Quest1)      
-
-
-bouncer1 = BouncingSprite("Hunter.png", 100, 100, 4, 2)
-bouncer2 = BouncingSprite("Hunter.png", 100, 100, -5, 3)
-bouncer3 = BouncingSprite("Hunter.png", 100, 100, 2,6)
-bouncer4 = BouncingSprite("Hunter.png", 100, 100, -3, 4)
-bouncer5 = BouncingSprite("Hunter.png", 100, 100, 4, -4)
-bouncer6 = BouncingSprite("Hunter.png", 100, 100, 3,4)
-bouncer7 = BouncingSprite("Hunter.png", 100, 100, -2, 3)
-bouncer8 = BouncingSprite("Hunter.png", 100, 100, 4, -1)
 
 
 all_sprites_Quest1_Bouncers = pygame.sprite.Group()
@@ -288,6 +252,38 @@ def draw_button(surface, rect, mouse_pos, button_text):
     surface.blit(text, (rect.centerx - text.get_width()//2,
                         rect.centery - text.get_height() // 2))
 
+# def draw_button(surface, rect, mouse_pos, button_text, x, y):
+
+#     text = font.render(button_text, True, BLACK)
+#     if rect.collidepoint(mouse_pos):
+#         color = SAND_HOVER
+#     else:
+#         color = SAND
+
+#     rect = pygame.Rect(x, y,)
+#     pygame.draw.rect(surface, color, rect, border_radius=10)
+#     surface.blit(text, (x, y))
+# def draw_button(surface, x, y, w, h, mouse_pos, button_text):
+#     # Create a rect from the given position and size
+#     rect = pygame.Rect(x, y, w, h)
+
+#     # Change color if hovering
+#     if rect.collidepoint(mouse_pos):
+#         color = SAND_HOVER
+#     else:
+#         color = SAND
+
+#     # Draw button
+#     pygame.draw.rect(surface, color, rect, border_radius=10)
+
+#     # Render and center text inside the rect
+#     text = font.render(button_text, True, BLACK)
+#     surface.blit(
+#         text, 
+#         (rect.centerx - text.get_width() // 2,
+#          rect.centery - text.get_height() // 2)
+#     )
+
 # Titlescreen Loop
 
 
@@ -310,7 +306,6 @@ quest3_group.add(quest3)
 
 quest1_end_time = 0  # buffer timer for ending quest1
 while True:
-
     # --- Event handling ---
     mouse_pos = pygame.mouse.get_pos()
     for event in pygame.event.get():
@@ -333,10 +328,12 @@ while True:
 
     # --- Titlescreen ---
     if runTitlescreen:
-        screen.blit(startBKG, (0,0))
-        start_button = Button(WIDTH //2 - 100, HEIGHT // 2 - 40, 200, 80,
+        screen.fill((240, 240, 220))
+        start_button = Button(WIDTH//2 - 100, HEIGHT//2 - 40, 200, 80,
                       "START", font, SAND, SAND_HOVER)
         start_button.draw(screen, mouse_pos)
+        # draw_button(screen, button_rect, mouse_pos, "START")
+        # draw_button(screen, 400, 300, 200, 200, button_rect, mouse_pos, "START")
 
     # --- Main Game ---
     elif runGame:
@@ -355,20 +352,11 @@ while True:
         # camera offset
         cam_x = max(0, min(player.rect.centerx - WIDTH//2, MAP_W - WIDTH))
         cam_y = max(0, min(player.rect.centery - HEIGHT//2, MAP_H - HEIGHT))
-        cam_x_dock = max(0, min(player.rect.centerx - WIDTH//2, MAP_W - WIDTH))
-        cam_y_dock = max(0, min(player.rect.centery - HEIGHT//2, MAP_H - HEIGHT))
 
-        
-
-        # draw map 
+        # draw map and player
         screen.fill((0, 0, 0))
         screen.blit(background, (-cam_x, -cam_y))
         player_display = player.rect.move(-cam_x, -cam_y)
-
-        # draw dock
-        screen.blit(dock.image, (dock.rect.x - cam_x+ 235, dock.rect.y - cam_y + 230))
-
-        #draw player
         screen.blit(player.image, player_display)
 
         if player.rect.collidepoint(550, 70 ) or player.rect.collidepoint(70, 310) or player.rect.collidepoint(1150, 300):
@@ -382,7 +370,6 @@ while True:
             # screen.blit(sign.image, (sign.rect.x - cam_x, sign.rect.y - cam_y))
             screen.blit(sign.image, (sign.rect.x - cam_x, sign.rect.y - cam_y))
 
-        
             
         keys = pygame.key.get_pressed()
         if player.rect.collidepoint(600,88) and keys[pygame.K_e]:
@@ -402,12 +389,6 @@ while True:
             quest3Menu = True
             runGame = False 
 
-        #dock
-        keys = pygame.key.get_pressed()
-        if player.rect.colliderect(dock.rect) and keys[pygame.K_e] and plankCount >= 20:
-            runWinScreen = True
-            runGame = False 
-
         # game over by timer
         if remaining_time <= 0:
             runGame = False
@@ -418,61 +399,26 @@ while True:
         wood_rect = wood_text.get_rect(topleft=(20, 20))
         screen.blit(wood_text, wood_rect)
 
-        
-
     # --- Quest1 ---
     elif quest1Menu:
-        screen.blit(menuBKG, (0, 0))
-
-        prompt_text = font.render("Quest One : Easy", True, (0,0,0))
-        prompt_rect = prompt_text.get_rect(center=(WIDTH // 2, HEIGHT -490))
-        screen.blit(prompt_text, prompt_rect)
-        prompt_text2 = font.render("Collect materials and avoid the Hunter", True, (0,0,0))
-        prompt_rect = prompt_text2.get_rect(center=(WIDTH // 2, HEIGHT -430))
-        screen.blit(prompt_text2, prompt_rect)
-
-        prompt_text3 = font.render("Enter either by sacrificing 15s or one wood", True, (0,0,0))
-        prompt_rect = prompt_text3.get_rect(center=(WIDTH // 2, HEIGHT -390))
-        screen.blit(prompt_text3, prompt_rect)
-
-        purchaseBtn = Button(WIDTH//2 - 145, HEIGHT//2 +10, 300, 80,
-                    "Sacrifice 15s", font, SAND, SAND_HOVER)
-        purchaseBtn.draw(screen, mouse_pos)
-        woodBtn = Button(WIDTH//2 - 145, HEIGHT//2 + 90, 300, 80,
-                    "Sacrifice one wood", font, SAND, SAND_HOVER)
-        woodBtn.draw(screen, mouse_pos) 
+        screen.fill((240, 240, 220))
+        purchaseBtn = Button(WIDTH//2 - 100, HEIGHT//2 - 40, 200, 80,
+                    "Enter Quest 1", font, SAND, SAND_HOVER)
+        purchaseBtn.draw(screen, mouse_pos) 
 
         if quest1Menu and purchaseBtn.is_clicked(event):
             quest1Menu = False
             runQuest1 = True
-            start_ticks -= 15000
-        elif quest1Menu and woodBtn.is_clicked(event):
-            quest1Menu = False
-            runQuest1 = True
-            plankCount -= 1
 
     elif runQuest1:
-        
-        if firstTime1:
-            firstTime1 = False 
-            starting_positions = [(100,100), (100,100), (100,100)]  # example positions
-            for i, bouncer in enumerate(all_sprites_Quest1_Bouncers.sprites()):
-                bouncer.rect.center = starting_positions[i]
-                bouncer.dx = random.choice([-5, -4, -3, 3, 4, 5])  # give them random initial velocities
-                bouncer.dy = random.choice([-5, -4, -3, 3, 4, 5])
-
         if not quest1_started:
-
-            for bouncer in all_sprites_Quest1_Bouncers:
-                bouncer.rect.center = (100, 100)  # fixed starting point
-
             quest1_started = True
             player.rect.center = (WIDTH//2, HEIGHT//2)
             planksKilled1 = 0
             # Clear old planks and spawn new ones
             all_wood_Quest1.empty()
             fillPlankGroup(planksInQuest1, all_wood_Quest1)
-
+        
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]: player.rect.y -= PLAYER_SPEED
@@ -493,7 +439,6 @@ while True:
         # check collisions with bouncers
         for bouncer in all_sprites_Quest1_Bouncers:
             if player.hitbox.colliderect(bouncer.hitbox):
-                firstTime1 = True
                 runQuest1 = False
                 runGame = True  # return to main game
 
@@ -504,16 +449,14 @@ while True:
                 planksKilled1 += 1
                 plankCount += 1
 
-        screen.blit(grassBKG, (0, 0))
-
         # finished quest1
         if planksKilled1 >= planksInQuest1:
-            firstTime1 = True
             runQuest1 = False
             runGame = True
             quest1_started = False
 
         # draw everything
+        screen.fill((240, 240, 220))
         all_wood_Quest1.draw(screen)
         all_sprites_Quest1_Bouncers.draw(screen)
         screen.blit(player.image, player.rect)
@@ -524,46 +467,17 @@ while True:
         screen.blit(wood_text, wood_rect)
     
     elif quest2Menu:
-        screen.blit(menuBKG, (0, 0))
-    
-        prompt_text4 = font.render("Quest Two : Medium", True, (0,0,0))
-        prompt_rect = prompt_text4.get_rect(center=(WIDTH // 2, HEIGHT -490))
-        screen.blit(prompt_text4, prompt_rect)
-        prompt_text5 = font.render("Collect the materials and avoid the Hunters", True, (0,0,0))
-        prompt_rect = prompt_text5.get_rect(center=(WIDTH // 2, HEIGHT -430))
-        screen.blit(prompt_text5, prompt_rect)
+        screen.fill((240, 240, 220))
+        purchaseBtn2 = Button(WIDTH//2 - 100, HEIGHT//2 - 40, 200, 80,
+                    "Enter Quest 2", font, SAND, SAND_HOVER)
+        purchaseBtn2.draw(screen, mouse_pos) 
 
-        prompt_text6 = font.render("Enter either by sacrificing 25s or two wood", True, (0,0,0))
-        prompt_rect = prompt_text6.get_rect(center=(WIDTH // 2, HEIGHT -390))
-        screen.blit(prompt_text6, prompt_rect)
-
-        purchaseBtn3 = Button(WIDTH//2 - 145, HEIGHT//2 +10, 300, 80,
-                    "Sacrifice 25s", font, SAND, SAND_HOVER)
-        purchaseBtn3.draw(screen, mouse_pos)
-        woodBtn2 = Button(WIDTH//2 - 145, HEIGHT//2 + 90, 300, 80,
-                    "Sacrifice two wood", font, SAND, SAND_HOVER)
-        woodBtn2.draw(screen, mouse_pos) 
-
-        if quest2Menu and purchaseBtn3.is_clicked(event):
+        if quest2Menu and purchaseBtn2.is_clicked(event):
             quest2Menu = False
             runQuest2 = True
-            start_ticks -= 25000
-        elif quest2Menu and woodBtn2.is_clicked(event):
-            quest2Menu = False
-            runQuest2 = True
-            plankCount -= 2
 
     # --- Quest2 
     elif runQuest2:
-        
-
-        if firstTime2:
-            firstTime2 = False 
-            starting_positions = [(100,100), (100,100), (100,100), (100,100), (100,100)]  # example positions
-            for i, bouncer in enumerate(all_sprites_Quest2_Bouncers.sprites()):
-                bouncer.rect.center = starting_positions[i]
-                bouncer.dx = random.choice([-5, -4, -3, 3, 4, 5])  # give them random initial velocities
-                bouncer.dy = random.choice([-5, -4, -3, 3, 4, 5])
 
         if not quest2_started:
             quest2_started = True
@@ -592,7 +506,6 @@ while True:
         # check collisions with bouncers
         for bouncer in all_sprites_Quest2_Bouncers:
             if player.hitbox.colliderect(bouncer.hitbox):
-                firstTime2 = True
                 runQuest2 = False
                 runGame = True  # return to main game
                 quest2_started = False
@@ -604,17 +517,14 @@ while True:
                 planksKilled2 += 1
                 plankCount += 1
 
-
-        screen.blit(grassBKG, (0, 0))
-
         # finished quest1
         if planksKilled2 >= planksInQuest2:
-            firstTime2 = True
             runQuest2 = False
             runGame = True
             quest2_started = False
 
         # draw everything
+        screen.fill((240, 240, 220))
         all_wood_Quest2.draw(screen)
         all_sprites_Quest2_Bouncers.draw(screen)
         screen.blit(player.image, player.rect)
@@ -625,48 +535,16 @@ while True:
         screen.blit(wood_text, wood_rect)
 
     elif quest3Menu:
-        screen.blit(menuBKG, (0, 0))
-
-        prompt_text = font.render("Quest Three : Hard", True, (0,0,0))
-        prompt_rect = prompt_text.get_rect(center=(WIDTH // 2, HEIGHT -490))
-        screen.blit(prompt_text, prompt_rect)
-        prompt_text2 = font.render("Collect the materials and avoid the Hunters", True, (0,0,0))
-        prompt_rect = prompt_text2.get_rect(center=(WIDTH // 2, HEIGHT -430))
-        screen.blit(prompt_text2, prompt_rect)
-
-        prompt_text3 = font.render("Enter either by sacrificing 35s or three wood", True, (0,0,0))
-        prompt_rect = prompt_text3.get_rect(center=(WIDTH // 2, HEIGHT -390))
-        screen.blit(prompt_text3, prompt_rect)
-
-        purchaseBtn3 = Button(WIDTH//2 - 145, HEIGHT//2 +10, 300, 80,
-                    "Sacrifice 35s", font, SAND, SAND_HOVER)
-        purchaseBtn3.draw(screen, mouse_pos)
-        woodBtn = Button(WIDTH//2 - 145, HEIGHT//2 + 90, 300, 80,
-                    "Sacrifice three wood", font, SAND, SAND_HOVER)
-        woodBtn.draw(screen, mouse_pos) 
+        screen.fill((240, 240, 220))
+        purchaseBtn3 = Button(WIDTH//2 - 100, HEIGHT//2 - 40, 200, 80,
+                    "Enter Quest 3", font, SAND, SAND_HOVER)
+        purchaseBtn3.draw(screen, mouse_pos) 
 
         if quest3Menu and purchaseBtn3.is_clicked(event):
             quest3Menu = False
             runQuest3 = True
-            start_ticks -= 35000
-        elif quest3Menu and woodBtn.is_clicked(event):
-            quest3Menu = False
-            runQuest3 = True
-            plankCount -= 3
-        
 
     elif runQuest3:
-        
-
-        if firstTime3:
-            firstTime3 = False 
-            starting_positions = [(100,100), (100,100), (100,100), (100,100), (100,100), (100,100), (100,100), (100,100)]  # example positions
-            for i, bouncer in enumerate(all_sprites_Quest3_Bouncers.sprites()):
-                bouncer.rect.center = starting_positions[i]
-                bouncer.dx = random.choice([-5, -4, -3, 3, 4, 5])  # give them random initial velocities
-                bouncer.dy = random.choice([-5, -4, -3, 3, 4, 5])
-
-
         if not quest3_started:
             quest3_started = True
             player.rect.center = (WIDTH//2, HEIGHT//2)
@@ -694,7 +572,6 @@ while True:
         # check collisions with bouncers
         for bouncer in all_sprites_Quest3_Bouncers:
             if player.hitbox.colliderect(bouncer.hitbox):
-                firstTime3 = True
                 runQuest3 = False
                 runGame = True  # return to main game
 
@@ -705,17 +582,14 @@ while True:
                 planksKilled3 += 1
                 plankCount += 1
 
-        screen.blit(grassBKG, (0, 0))
-
         # finished quest3
         if planksKilled3 >= planksInQuest3:
-            firstTime3 = True
             runQuest3 = False
             runGame = True
             quest3_started = False
 
         # draw everything
-        # screen.fill((240, 240, 220))
+        screen.fill((240, 240, 220))
         all_wood_Quest3.draw(screen)
         all_sprites_Quest3_Bouncers.draw(screen)
         screen.blit(player.image, player.rect)
@@ -726,22 +600,12 @@ while True:
         screen.blit(wood_text, wood_rect)
 
 
-    elif runWinScreen:
-        screen.blit(winBKG, (0, 0))
-        # win_text = font.render("You Won!!", True, (0, 0, 0))  
-        # center the text on the screen
-        # screen.blit(win_text, (WIDTH//2 - win_text.get_width()//2, HEIGHT//2 - win_text.get_height()//2))
-    
-       # pygame.display.flip() 
-
-
-
     # --- Endscreen ---
     elif runEndscreen:
-        screen.blit(loseBKG, (0, 0))
-        #draw_button(screen, button_rect, mouse_pos, "Restart")
-        # end_text = font.render("Game Over", True, (0, 0, 0))
-        # screen.blit(end_text, (WIDTH//2 - end_text.get_width()//2, HEIGHT//2 - end_text.get_height()//2))
+        screen.fill((0,0,0))
+        draw_button(screen, button_rect, mouse_pos, "Restart")
+        end_text = font.render("Game Over", True, (255,255,255))
+        screen.blit(end_text, (WIDTH//2 - end_text.get_width()//2, HEIGHT//2 - end_text.get_height()//2))
 
     # --- Update display ---
     pygame.display.flip()
